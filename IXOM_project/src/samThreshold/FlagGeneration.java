8453eb027be862ea2ba66acda30faeaa03d62086
magratsam <magratsam@magratsam-VirtualBox>
2016-12-12 14:26:01 +1300

    debugged FlagGeneration

--- IXOM_project/src/samThreshold/FlagGeneration.java
+++ IXOM_project/src/samThreshold/FlagGeneration.java
@@ -3,39 +3,57 @@ package samThreshold;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import Read_Data.CSV2Array;
 import Read_Data.dataPoint;
 import henrysThreshold.metricTaker;
 
+/**
+ * @author magratsam
+ *
+ */
+
+
 public class FlagGeneration {
+	
+
 	
 	private static ArrayList<dataPoint> data;
 	private metricTaker metric;
 	private static File input;
 	
-	public static void main(String[] args) throws FileNotFoundException {
-		
-		input = new File("/home/magratsam/git/IXOM_project/IXOM_project/src/Read_Data.cleanfile.csv");
-		CSV2Array c = new CSV2Array();
-		c.populateData(input, data);
-		for(dataPoint d: data){
-			System.out.println(d.print());
-			
-		}
-	}
-	
 	public FlagGeneration(ArrayList<dataPoint> data) {
 		
 		this.data = data;
-		metricTaker metric = new metricTaker(data);
+		metric = new metricTaker(data);
 	}
 	
-	public void Flag1() {
+	public void flagOne() {
 		
-		for (int i = 0; i < metric.getCondAverages().size(); i++) {
-			System.out.println(metric.getCondAverages().get(i));
+		HashMap<Integer,Double> condAverages = new HashMap<Integer,Double>();
+		
+		metric.getCondAverages();
+
+		System.out.println("hi");
+		
+		
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		input = new File("/home/magratsam/git/cleanfile5.csv");
+		CSV2Array c = new CSV2Array();
+		
+		
+		data = c.populateData(input, data);
+		
+		for(dataPoint d: data){
+			System.out.println(d.print());
 		}
+		
+		FlagGeneration f = new FlagGeneration(data);
+		f.flagOne();
 		
 	}
 	
