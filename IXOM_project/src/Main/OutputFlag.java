33e2ea329d250d535d04f90f1a37e86e1531765d
coulsohenr <coulsohenr@coulsohenr-VirtualBox>
2016-12-12 14:27:37 +1300

    intermediate commit

--- IXOM_project/src/Main/OutputFlag.java
+++ IXOM_project/src/Main/OutputFlag.java
@@ -1,7 +1,18 @@
 package Main;
+import java.util.ArrayList;
 
+import Read_Data.*;
 
 
 public class OutputFlag {
 
+	
+	
+	public static String generateFlagStrings(ArrayList<dataPoint> data){
+		
+		
+		
+		return "flags to go here";
+		
+	}
 }
