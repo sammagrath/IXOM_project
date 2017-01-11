d041c8c8f72afe13f2ff3af04216db143172144c
hofflajaco <hofflajaco@hofflajaco-VirtualBox>
2017-01-11 15:15:33 +1300

    Starting Analyser Class and created Coordinate object

--- IXOM_project/src/decayAnalysis/Analyser.java
+++ IXOM_project/src/decayAnalysis/Analyser.java
@@ -1,5 +1,17 @@
 package decayAnalysis;
 
-public class Analyser {
+import java.util.ArrayList;
 
+import Read_Data.dataPoint;
+
+public class Analyser {
+	
+	public ArrayList<Coordinate> findSteepest(ArrayList<dataPoint> dataPoints){
+		
+		ArrayList<Coordinate> steepest = new ArrayList<>();
+		
+		return steepest;
+		
+	}
+	
 }
