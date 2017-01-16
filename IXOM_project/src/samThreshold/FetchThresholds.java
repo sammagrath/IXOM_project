d461588988995d4db1dc9ea59dca5f971776af2e
magratsam <magratsam@gs1615.AD.ECS.VUW.AC.NZ>
2017-01-16 15:03:04 +1300

    Work in progress - populating hashmap is a bitch

--- IXOM_project/src/samThreshold/FetchThresholds.java
+++ IXOM_project/src/samThreshold/FetchThresholds.java
@@ -16,31 +16,124 @@ import Read_Data.ExceltoCSV;
 
 public class FetchThresholds {
 
-	private static ArrayList<Double> thresholds = new ArrayList();
-	private static HashMap<String, ArrayList<Double>> powders = new HashMap();
+	private static ArrayList<Threshold> thresholds = new ArrayList();
+	private static HashMap<String, ArrayList<Threshold>> powders = new HashMap();
 	private static Threshold t;
+	
 
-	public void setThresholds(String input, String product, String process) throws FileNotFoundException {
+	public void setThresholds(String input, String product, String process) throws IOException {
 
 		File excel = new File(input);
 
-		Scanner scanner = new Scanner(excel);
-		scanner.useDelimiter(",");
-		while (scanner.hasNextLine()) {
-
-			if (scanner.next().equals(process)) {
-
-				while (scanner.hasNextDouble()) {
-					thresholds.add(scanner.nextDouble());
-				}
-				powders.put(process, thresholds);
-
+		Scanner tScan = new Scanner(excel);
+		tScan.useDelimiter(",");
+		
+		BufferedReader brTest = new BufferedReader(new FileReader(System.getProperty("user.dir")+ File.separator + "thresholds.csv"));
+		String text = brTest.readLine();
+		
+		Scanner pScan = new Scanner(text);
+		pScan.useDelimiter(",");
+		while(pScan.hasNext()) {
+			Threshold t = new Threshold();
+			t.setPhase(pScan.next());
+			thresholds.add(t);
+		}
+		String currentLine = "";
+		String currentItem = "";
+		String saveItem = "";
+		
+		int count = 1, index = 0;
+		currentItem = tScan.nextLine();
+		Threshold currentThreshold = new Threshold();
+		ArrayList<Threshold> tempThresholds = new ArrayList<Threshold>();
+		
+		while(tScan.hasNextLine()) {
+			tempThresholds = new ArrayList<Threshold>();
+//			currentLine = tScan.nextLine();
+//			while(tScan.hasNextDouble()) {
+//				for(Threshold t:thresholds) {
+//					t.setCondLower(tScan.nextDouble());
+//					t.setCondUpper(tScan.nextDouble());
+//					t.setTempLower(tScan.nextDouble());
+//					t.setTempUpper(tScan.nextDouble());
+//					System.out.println(t.printThresholds());
+//				}
+//			}
+			
+			currentThreshold = thresholds.get(index);
+			
+			if((count-2) % 18 ==0){
+				saveItem = tScan.next();
+				currentThreshold.setPhase(saveItem);
+				System.out.println(saveItem);
+				
+			} else if((count-3 % 18 ==0) || (count-7 % 18 ==0) || (count-11 % 18 ==0) || (count-15 % 18 ==0)){
+				currentThreshold.setCondLower(tScan.nextDouble());
+			} else if((count-4 % 18 ==0) || (count-8 % 18 ==0) || (count-12 % 18 ==0) || (count-16 % 18 ==0)){
+				currentThreshold.setCondUpper(tScan.nextDouble());
+			} else if((count-5 % 18 ==0) || (count-9 % 18 ==0) || (count-13 % 18 ==0) || (count-17 % 18 ==0)){
+				currentThreshold.setTempLower(tScan.nextDouble());
+			} else if((count-6 % 18 ==0) || (count-10 % 18 ==0) || (count-12 % 18 ==0) || (count % 18 ==0)){
+				currentThreshold.setTempUpper(tScan.nextDouble());
+				tempThresholds.add(currentThreshold);
+				index++;
+			}else {
+				currentItem = tScan.next();
+			}
+				
+			
+			count++;
+			
+			if((count-1) % 18 == 0){
+				ArrayList<Threshold> toMap = tempThresholds;
+				powders.put(saveItem, toMap);
 			}
 		}
-		scanner.close();
-		for (Double d : thresholds) {
-			System.out.println(d);
+		
+		
+//		System.out.println(powders.size());
+		
+//		for(Threshold d : thresholds) {
+//			System.out.print(d.getPhase());
+//			
+//			while(tScan.hasNextLine()) {
+//				
+//				if(tScan.next().equals(process)) {
+//					
+//					d.setCondLower(tScan.nextDouble());
+//					d.setCondUpper(tScan.nextDouble());
+//					d.setTempLower(tScan.nextDouble());
+//					d.setTempUpper(tScan.nextDouble());
+//				}
+//			}
+//		}
+		
+		for(String s : powders.keySet()) {
+			
+			String key = s.toString();
+			
+			System.out.println(key + " " +powders.get(s).get(0).printThresholds());
+			
+//			System.out.println("Key: " + key + ", Value: " + value);
 		}
+		
+		
+		
+//		while (scanner.hasNextLine()) {
+//
+//			if (scanner.next().equals(process)) {
+//
+//				while (scanner.hasNextDouble()) {
+//					thresholds.add(scanner.nextDouble());
+//				}
+//				powders.put(process, thresholds);
+//
+//			}
+//		}
+//		scanner.close();
+//		for (Double d : thresholds) {
+//			System.out.println(d);
+//		}
 		System.out.println("\n" + powders.keySet());
 		System.out.println(powders.get(process));
 
