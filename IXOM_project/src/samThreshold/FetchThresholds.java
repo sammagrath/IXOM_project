c36b411ff277433eb8cca7eb114254eea4986c39
magratsam <magratsam@gs1615.AD.ECS.VUW.AC.NZ>
2017-01-25 11:13:35 +1300

    Added some comments, removed main method from FetchThresholds class

--- IXOM_project/src/samThreshold/FetchThresholds.java
+++ IXOM_project/src/samThreshold/FetchThresholds.java
@@ -12,24 +12,26 @@ public class FetchThresholds {
 
 	private static ArrayList<String> processes = new ArrayList<>();
 	private static HashMap<String, ArrayList<Threshold>> powders = new HashMap<String, ArrayList<Threshold>>();
-
+	
+	//Method for generating, populating and returning HashMap containing thresholds for use by Flag Generation class
 	public HashMap<String, ArrayList<Threshold>> setThresholds(String input) throws IOException {
-		
+
 		String processName = "";
-		
+
 		ArrayList<Threshold> thresholds = new ArrayList<>();
-		
+
 		File excel = new File(input);
 
-		
-		//Converts first line of CSV file to string. String is then scanned and each token is used to set phase names of newly generated Threshold objects, 
-		//which are then added to a threshold arrayList
+		// Converts first line of CSV file to string. String is then scanned and
+		// each token is used to set phase names of newly generated Threshold
+		// objects,
+		// which are then added to a threshold arrayList
 		BufferedReader brTest = new BufferedReader(
 				new FileReader(System.getProperty("user.dir") + File.separator + "thresholds.csv"));
 
 		String text = brTest.readLine();
 		brTest.close();
-		
+
 		text = removeCommas(text);
 		Scanner pScan = new Scanner(text);
 		pScan.useDelimiter(",");
@@ -40,9 +42,10 @@ public class FetchThresholds {
 			thresholds.add(t);
 		}
 		pScan.close();
-		
-		//Scanner initialised, scans CSV file and populates processes arrayList with the name of each process, which is
-		//then used as reference for populating hashmap
+
+		// Scanner initialised, scans CSV file and populates processes arrayList
+		// with the name of each process, which is
+		// then used as reference for populating hashmap
 		Scanner nScan = new Scanner(excel);
 		nScan.useDelimiter(",");
 		nScan.nextLine();
@@ -59,9 +62,11 @@ public class FetchThresholds {
 
 		}
 		nScan.close();
-		
-		//CSV file is scanned process name matches in the process name arrayList, when a match is found,
-		//the threshold array is populated with the associated threshold values pertaining to that process
+
+		// CSV file is scanned process name matches in the process name
+		// arrayList, when a match is found,
+		// the threshold array is populated with the associated threshold values
+		// pertaining to that process
 		for (String s : processes) {
 			Scanner tScan = new Scanner(excel);
 			tScan.useDelimiter(",");
@@ -82,7 +87,7 @@ public class FetchThresholds {
 						t.setCondUpper(tScan.nextDouble());
 						t.setTempLower(tScan.nextDouble());
 						t.setTempUpper(tScan.nextDouble());
-//						System.out.println(t.printThresholds());
+						// System.out.println(t.printThresholds());
 
 					}
 					System.out.println();
@@ -94,49 +99,41 @@ public class FetchThresholds {
 			}
 			tScan.close();
 		}
-		
-		//print function for validating hashmap population
-//		for (String s : powders.keySet()) {
-//
-//			String key = s.toString();
-//
-//			System.out.println(key + " " + powders.get(s).get(0).printThresholds());
-//
-//		}
-		
+
+		// print function for validating hashmap population
+		// for (String s : powders.keySet()) {
+		//
+		// String key = s.toString();
+		//
+		// System.out.println(key + " " +
+		// powders.get(s).get(0).printThresholds());
+		//
+		// }
+
 		return powders;
 
 	}
-
-	public static void main(String[] args) throws IOException {
-
-		FetchThresholds ft = new FetchThresholds();
-		System.out.println(System.getProperty("user.dir") + File.separator + "thresholds.csv");
-
-		ft.setThresholds(System.getProperty("user.dir") + File.separator + "thresholds.csv");
-
-	}
 	
-	
-	public String removeCommas(String toClean){
+	//Jacob's addition - method for cleaning unncessary commas from uploaded threshold csv file, ensuring accurate population of HashMap
+	public String removeCommas(String toClean) {
 		Boolean keepComma = false;
 		String toReturn = "";
 		int length = toClean.length();
 		char c;
-		
-		if(length > 0){
-			for(int i = 0; i < length; i++){
+
+		if (length > 0) {
+			for (int i = 0; i < length; i++) {
 				c = toClean.charAt(i);
-				if(c != ','){
+				if (c != ',') {
 					toReturn += c;
 					keepComma = true;
-				} else if (keepComma == true){
+				} else if (keepComma == true) {
 					toReturn += c;
-					keepComma = false;	
+					keepComma = false;
 				}
 			}
 		}
-		
+
 		return toReturn;
 	}
 }
